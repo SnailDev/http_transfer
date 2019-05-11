5cb1083f201a62239ff8e7d21630cf4e1bea8bbb
snaildev <snailtem@gmail.com>
2019-05-11 10:12:11 +0800

    writeurl.json

--- app.js
+++ app.js
@@ -1,18 +1,29 @@
 var http = require('http');
+var request = require('request')
 var httpProxy = require('http-proxy');
 var querystring = require('querystring');
+var url = require("url");
 
 var proxy = httpProxy.createProxyServer({});
 
 http.createServer(function (req, res) {
-    delete req.headers.host;
+    // delete req.headers.host;
     var query = querystring.parse(req.url.replace("/?", ""));
-    req.url = "/";
-    proxy.web(req, res, {
-        target: query.url,
-        changeOrigin: true
-    }, (e) => {
-        console.log("proxy error call back ");
-        console.log(e);
+    request("https://raw.githubusercontent.com/SnailDev/http_transfer/master/writeurl.json", function (error, response, body) {
+        console.log(`响应主体: ${body}`);
+        var urlObj = url.parse(query.url);
+        if (body.indexOf(urlObj.hostname)) {
+            req.url = "/";
+            proxy.web(req, res, {
+                target: query.url,
+                changeOrigin: true
+            }, (e) => {
+                console.log("----代理返回异常----");
+                console.log(e);
+            });
+        } else {
+            console.log("---非法请求---");
+        }
     });
+
 }).listen(3003);
