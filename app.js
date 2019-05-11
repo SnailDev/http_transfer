73fbedada9c3f6f36def8379fc56c360333fe8dd
snaildev <snailtem@gmail.com>
2019-05-11 10:16:19 +0800

    白名单设置

--- app.js
+++ app.js
@@ -1,28 +1,31 @@
-var http = require('http');
-var request = require('request')
-var httpProxy = require('http-proxy');
-var querystring = require('querystring');
-var url = require("url");
+const url = require("url");
+const http = require('http');
+const request = require('request')
+const httpProxy = require('http-proxy');
+const querystring = require('querystring');
+
+var hostsUrl = "https://raw.githubusercontent.com/SnailDev/http_transfer/master/hosts.json";
 
 var proxy = httpProxy.createProxyServer({});
-
 http.createServer(function (req, res) {
-    // delete req.headers.host;
-    var query = querystring.parse(req.url.replace("/?", ""));
-    request("https://raw.githubusercontent.com/SnailDev/http_transfer/master/writeurl.json", function (error, response, body) {
-        console.log(`响应主体: ${body}`);
-        var urlObj = url.parse(query.url);
-        if (body.indexOf(urlObj.hostname)) {
-            req.url = "/";
-            proxy.web(req, res, {
-                target: query.url,
-                changeOrigin: true
-            }, (e) => {
-                console.log("----代理返回异常----");
-                console.log(e);
-            });
-        } else {
-            console.log("---非法请求---");
+    var queryStr = querystring.parse(req.url.replace("/?", ""));
+    request(hostsUrl, function (error, response, body) {
+        if (!error) {
+            var urlObj = url.parse(queryStr.url);
+            if (body.indexOf(urlObj.hostname)) {
+                req.url = "/";
+                proxy.web(req, res, {
+                    target: queryStr.url,
+                    changeOrigin: true
+                }, (e) => {
+                    console.log("----代理返回异常----");
+                    console.log(e);
+                });
+            } else {
+                console.log("---非法请求---");
+            }
+        }else{
+            console.log("----github 响应异常----")
         }
     });
 
